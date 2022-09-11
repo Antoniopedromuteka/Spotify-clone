1b3d11251e9838c668353bf4b2a2e242ac8f80a7
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-09-11 17:40:11 +0100

    adicionando algumas padronizacoes no projecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        "14": "3.5rem",
+        "125px": "125px"
+      },
+      colors: {
+        "hoverspt": "#18d760",
+        "purple-main": "#2d46b9",
+        "green-main": "#1ed760"
+      },
+      backgroundImage: theme =>({
+        "spotify-theme": "url('/img/brusts.svg')",
+        "spotify-theme-mobile": "url('/img/brusts-mobile.svg')"
+      }),
+      backgroundSize:{
+        "177%": "175%",
+        "195%": "195%"
+      },
+      backgroundPosition:{
+        "banner": "46% 4%",
+        "banner-mobile": "top 25% center"
+      },
+      fontSize:{
+        "9xl": "9rem"
+      }
+    },
   },
   plugins: [],
 }
